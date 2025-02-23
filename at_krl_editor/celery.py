11764d043aa3566b00ce999d2f111cc523a47bf5
grigandal625 <grigandal625@gmail.com>
2025-02-23 11:47:48 +0300

    fix celery

--- at_krl_editor/celery.py
+++ at_krl_editor/celery.py
@@ -1,10 +1,9 @@
 import os
 
 from celery import Celery
+from at_krl_editor.utils.settings import get_django_settings_module
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_SETTINGS_MODULE", "at_krl_editor.base_server.settings")
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", get_django_settings_module())
 
 app = Celery("at_krl_editor")
 app.config_from_object("django.conf:settings", namespace="CELERY")
