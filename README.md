e14c631136a5d0cd1eab87c58d42f052ec6456a4
grigandal625 <grigandal625@gmail.com>
2025-03-10 21:58:21 +0300

    extra add

---
+++ README.md
@@ -0,0 +1,7 @@
+# Установка
+
+С помощью `poetry`
+
+```
+poetry add --group base_server git+https://github.com/grigandal625/AT_KRL_EDITOR@master
+```
--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 import os
 import shutil
-
 from setuptools import setup
 from setuptools.command.install import install
-
 
 class PostInstallCommand(install):
     def run(self):
         install.run(self)
+        
+        # Проверяем активацию extra через флаги установки
+        base_server_enabled = any(
+            'base_server' in flag.lower()
+            for flag in self.distribution.install_requires
+        )
 
-        base_server_path = os.path.join(os.path.dirname(__file__), "at_krl_editor", "base_server")
+        base_server_src = os.path.join(os.path.dirname(__file__), "at_krl_editor", "base_server")
+        dest = os.path.join(os.getcwd(), "base_server")
 
-        if "base_server" in self.distribution.extras_require:
-            dest = os.path.join(os.getcwd(), "base_server")
-            shutil.copytree(base_server_path, dest)
-            print(f"Django project template copied to {dest}")
+        if base_server_enabled:
+            if os.path.exists(base_server_src):
+                shutil.copytree(base_server_src, dest, dirs_exist_ok=True)
+                print(f"Django template copied to {dest}")
+            else:
+                print(f"Warning: base_server template not found at {base_server_src}")
         else:
-            if os.path.exists(base_server_path):
-                shutil.rmtree(base_server_path)
-                print(f"Deleted {base_server_path} because 'base_server' extra was not specified.")
-
+            if os.path.exists(dest):
+                shutil.rmtree(dest)
+                print(f"Cleaned {dest} as 'base_server' extra was not requested")
 
 setup(
     cmdclass={
